9da652cfc1b9c41988c9cea93f0091b44be47454
cajgomez <Jgomez@cambridgeassociates.com>
2020-03-16 20:30:01 -0600

    Updating dependencies, adding requestTest.http test.

--- src/main/java/com/example/demofunction/DemoFunctionApplication.java
+++ src/main/java/com/example/demofunction/DemoFunctionApplication.java
@@ -2,18 +2,21 @@ package com.example.demofunction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.function.context.FunctionScan;
 import org.springframework.context.annotation.Bean;
 
 import java.util.function.Function;
 
 @SpringBootApplication
-@FunctionScan
 public class DemoFunctionApplication {
 
 	@Bean
 	public Function<String, String> upperCase(){
 		return name -> name.toUpperCase();
+	}
+
+	@Bean
+	public Function<Person, String> upperCaseName(){
+		return Person -> Person.getName().toUpperCase();
 	}
 
 	public static void main(String[] args) {
