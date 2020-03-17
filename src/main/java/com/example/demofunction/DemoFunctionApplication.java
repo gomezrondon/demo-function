227dd211e7d292a346272a9667c1113c9a86d34a
cajgomez <Jgomez@cambridgeassociates.com>
2020-03-17 06:39:43 -0600

    Adding cloud function definition.

--- src/main/java/com/example/demofunction/DemoFunctionApplication.java
+++ src/main/java/com/example/demofunction/DemoFunctionApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class DemoFunctionApplication {
 
 	@Bean
-	public Function<String, String> upperCase(){
+	public Function<String, String> uppercase(){
 		return name -> name.toUpperCase();
 	}
 
@@ -18,6 +20,17 @@ public class DemoFunctionApplication {
 	public Function<Person, String> upperCaseName(){
 		return Person -> Person.getName().toUpperCase();
 	}
+
+	@Bean
+	public Supplier< String> time(){
+		return () -> LocalDateTime.now().toString();
+	}
+
+	@Bean
+	public Function<String, String> reverse(){
+		return text -> new StringBuilder(text).reverse().toString();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoFunctionApplication.class, args);
