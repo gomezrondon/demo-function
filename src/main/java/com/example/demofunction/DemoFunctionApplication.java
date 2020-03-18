835b7e740def6ad7eb36fb3f4d58da85147f0eaf
cajgomez <Jgomez@cambridgeassociates.com>
2020-03-18 13:50:34 -0600

    Car service example.

--- src/main/java/com/example/demofunction/DemoFunctionApplication.java
+++ src/main/java/com/example/demofunction/DemoFunctionApplication.java
@@ -1,14 +1,20 @@
 package com.example.demofunction;
 
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.hateoas.CollectionModel;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -18,6 +24,9 @@ public class DemoFunctionApplication {
 
 	@Autowired
 	private NameService nameService;
+
+	@Autowired
+    private CarClient carClient;
 
 	@Bean
 	public Function<String, String> uppercase(){
@@ -41,6 +50,19 @@ public class DemoFunctionApplication {
 		return () -> nameService.getTime();
 	}
 
+
+	public Collection<Car> fallbackGetCars() {
+		return new ArrayList<Car>();
+	}
+
+    @Bean
+	@HystrixCommand(fallbackMethod = "fallbackGetCars")
+    Supplier<Collection<Car>> getcars() {
+	    return () -> carClient.getCars()
+				.getContent();
+    }
+
+
 	@Bean
 	public Function<String, String> reverse(){
 		return text -> new StringBuilder(text).reverse().toString();
@@ -50,4 +72,17 @@ public class DemoFunctionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoFunctionApplication.class, args);
 	}
+}
+
+@Data
+class Car{
+    private String name;
+
+}
+
+@FeignClient(url = "http://localhost:8090", name = "car-service")
+interface CarClient{
+
+    @GetMapping("/cars")
+	CollectionModel<Car> getCars();
 }
